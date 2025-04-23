29d40a1033b8b8bd16fd17988a7fcdd84ba64402
Asep Irman <asepirman@antigravity.id>
2025-04-23 22:07:32 +0700

    all path

--- id_en/products/cnt/plcr/pmerit/concept/assets/js/rollover.js
+++ id_en/products/cnt/plcr/pmerit/concept/assets/js/rollover.js
@@ -6,18 +6,18 @@ for( i = 0 ; i < 11 ; i++ ){
 rolimg[i] = new Image();
 }
 
-// Œ³‰æ‘œ‚ÌƒCƒ[ƒW‚ÌƒpƒX
-rolimg[0].src= "/fa/products/cnt/plcr/pmerit/concept/img/concept_circle00.jpg"
-rolimg[1].src= "/fa/products/cnt/plcr/pmerit/concept/img/concept_circle01.jpg"
-rolimg[2].src= "/fa/products/cnt/plcr/pmerit/concept/img/concept_circle02.jpg"
-rolimg[3].src= "/fa/products/cnt/plcr/pmerit/concept/img/concept_circle03.jpg"
-rolimg[4].src= "/fa/products/cnt/plcr/pmerit/concept/img/concept_circle04.jpg"
-rolimg[5].src= "/fa/products/cnt/plcr/pmerit/concept/img/concept_circle05.jpg"
-rolimg[6].src= "/fa/products/cnt/plcr/pmerit/concept/img/concept_circle06.jpg"
-rolimg[7].src= "/fa/products/cnt/plcr/pmerit/concept/img/concept_circle07.jpg"
-rolimg[8].src= "/fa/products/cnt/plcr/pmerit/concept/img/concept_circle08.jpg"
-rolimg[9].src= "/fa/products/cnt/plcr/pmerit/concept/img/concept_circle09.jpg"
-rolimg[10].src= "/fa/products/cnt/plcr/pmerit/concept/img/concept_circle10.jpg"}
+// ï¿½ï¿½ï¿½æ‘œï¿½ÌƒCï¿½ï¿½ï¿½[ï¿½Wï¿½Ìƒpï¿½X
+rolimg[0].src= "/id_en/products/cnt/plcr/pmerit/concept/img/concept_circle00.jpg"
+rolimg[1].src= "/id_en/products/cnt/plcr/pmerit/concept/img/concept_circle01.jpg"
+rolimg[2].src= "/id_en/products/cnt/plcr/pmerit/concept/img/concept_circle02.jpg"
+rolimg[3].src= "/id_en/products/cnt/plcr/pmerit/concept/img/concept_circle03.jpg"
+rolimg[4].src= "/id_en/products/cnt/plcr/pmerit/concept/img/concept_circle04.jpg"
+rolimg[5].src= "/id_en/products/cnt/plcr/pmerit/concept/img/concept_circle05.jpg"
+rolimg[6].src= "/id_en/products/cnt/plcr/pmerit/concept/img/concept_circle06.jpg"
+rolimg[7].src= "/id_en/products/cnt/plcr/pmerit/concept/img/concept_circle07.jpg"
+rolimg[8].src= "/id_en/products/cnt/plcr/pmerit/concept/img/concept_circle08.jpg"
+rolimg[9].src= "/id_en/products/cnt/plcr/pmerit/concept/img/concept_circle09.jpg"
+rolimg[10].src= "/id_en/products/cnt/plcr/pmerit/concept/img/concept_circle10.jpg"}
 
 function paintRol(dim,cnt){
 if(navigator.appVersion.charAt(0) >= 3 ){
@@ -25,7 +25,7 @@ document.images[dim].src=rolimg[cnt].src;
 }
 }
 
-//ƒCƒ[ƒWƒ}ƒbƒv‚Ì‹«ŠEü‚ðÁ‚·
+//ï¿½Cï¿½ï¿½ï¿½[ï¿½Wï¿½}ï¿½bï¿½vï¿½Ì‹ï¿½ï¿½Eï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
 var ua = navigator.userAgent;
 var chkFF = ua.indexOf("Firefox",0);
 if(chkFF>0){
