f5231eb762e1c0ff734c48ae6fb78c1ed57894d6
Cecep Aprilianto <cecepaprilianto@gmail.com>
2025-05-24 10:22:17 +0700

    feat: finish page event - MEIN - Showroom Cikarang

--- id_id/events-seminars/showroom-cikarang/data.js
+++ id_id/events-seminars/showroom-cikarang/data.js
@@ -2,72 +2,72 @@ let datas = [
     {
       "URL": "/id_id/events-seminars/showroom-cikarang/B-1.html",
       "Image": "/id_id/events-seminars/assets/img/showroom/B-1.jpg",
-      "Title": "Digitalization of Smart Manufacturing for Production Line"
+      "Title": "Digitalisasi Manufaktur Cerdas untuk Lini Produksi"
     },
     {
       "URL": "/id_id/events-seminars/showroom-cikarang/B-2.html",
       "Image": "/id_id/events-seminars/assets/img/showroom/B-2.jpg",
-      "Title": "Collaborative Robot Barista"
+      "Title": "Robot Barista Kolaboratif"
     },
     {
       "URL": "/id_id/events-seminars/showroom-cikarang/B-3.html",
       "Image": "/id_id/events-seminars/assets/img/showroom/B-3.jpg",
-      "Title": "HVAC Redundancy Control System"
+      "Title": "Sistem Kontrol Redundansi HVAC"
     },
     {
       "URL": "/id_id/events-seminars/showroom-cikarang/B-4.html",
       "Image": "/id_id/events-seminars/assets/img/showroom/B-4.jpg",
-      "Title": "Induction Motor"
+      "Title": "Motor Induksi"
     },
     {
       "URL": "/id_id/events-seminars/showroom-cikarang/B-5.html",
       "Image": "/id_id/events-seminars/assets/img/showroom/B-5.jpg",
-      "Title": "Engraving CNC Demo Machine"
+      "Title": "Mesin Demo Ukiran CNC"
     },
     {
       "URL": "/id_id/events-seminars/showroom-cikarang/B-6.html",
       "Image": "/id_id/events-seminars/assets/img/showroom/B-6.jpg",
-      "Title": "High-performance Wire-cut EDM MV-R Series"
+      "Title": "Kawat potong berkinerja tinggi EDM MV-R Series"
     },
     {
       "URL": "/id_id/events-seminars/showroom-cikarang/B-7.html",
       "Image": "/id_id/events-seminars/assets/img/showroom/B-7.jpg",
-      "Title": "High-performance Die-Sinker EDM SG Series"
+      "Title": "Die-Sinker Berkinerja Tinggi EDM SG Series"
     },
     {
       "URL": "/id_id/events-seminars/showroom-cikarang/B-8.html",
       "Image": "/id_id/events-seminars/assets/img/showroom/B-8.jpg",
-      "Title": "Pick and Place Robot Candy"
+      "Title": "Pilih dan Tempatkan Permen Robot"
     },
     {
       "URL": "/id_id/events-seminars/showroom-cikarang/B-9.html",
       "Image": "/id_id/events-seminars/assets/img/showroom/B-9.jpg",
-      "Title": "HVAC Application for Clean Room and Control Room with SCADA System"
+      "Title": "Aplikasi HVAC untuk Ruang Bersih dan Ruang Kontrol dengan Sistem SCADA"
     },
     {
       "URL": "/id_id/events-seminars/showroom-cikarang/B-10.html",
       "Image": "/id_id/events-seminars/assets/img/showroom/B-10.jpg",
-      "Title": "Oil and Gas Solution"
+      "Title": "Solusi Minyak dan Gas"
     },
     {
       "URL": "/id_id/events-seminars/showroom-cikarang/B-11.html",
       "Image": "/id_id/events-seminars/assets/img/showroom/B-11.jpg",
-      "Title": "HVAC Application for Social Infrastructure"
+      "Title": "Aplikasi HVAC untuk Infrastruktur Sosial"
     },
     {
       "URL": "/id_id/events-seminars/showroom-cikarang/B-12.html",
       "Image": "/id_id/events-seminars/assets/img/showroom/B-12.jpg",
-      "Title": "Carbon Neutral Solution (Energy Monitoring System)"
+      "Title": "Solusi Netral Karbon (Sistem Pemantauan Energi)"
     },
     {
       "URL": "/id_id/events-seminars/showroom-cikarang/B-13.html",
       "Image": "/id_id/events-seminars/assets/img/showroom/B-13.jpg",
-      "Title": "CNC Demokit No-2 In-house Repair"
+      "Title": "Perbaikan Internal CNC Demokit No-2"
     },
     {
       "URL": "/id_id/events-seminars/showroom-cikarang/B-14.html",
       "Image": "/id_id/events-seminars/assets/img/showroom/B-14.jpg",
-      "Title": "Pick and Place Robot with Rotary and Linear Synchronize Application"
+      "Title": "Robot Pick and Place dengan Aplikasi Sinkronisasi Rotary dan Linear"
     }
 ];
 
